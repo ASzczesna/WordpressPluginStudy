0850b602d8d04ba639ceecc3ecc6d8b650f9d7e7
ASzczesna <szczesna.aleksandra@gmail.com>
2016-09-23 20:25:06 +0200

    dodawanie G.A. prosto do kodu

---
+++ MyGoogleAnalytics/MyGoogleAnalytics.php
@@ -0,0 +1,32 @@
+<?php
+/*
+Plugin Name: MyGoogleAnalytics
+Plugin URI: 
+Description: Dodaje kod G.A. prosto na górê headera
+Version: 0.1 
+Requires at least: 1.0
+Author: Aleksandra Szczêsna
+Author URI: http://mojswiat.eu
+License: GPL2
+*/
+
+add_action( 'wp_head', 'GAcode', 0 );
+
+function GAcode() { ?>
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-XXXXXXX-X', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
+<?php 
+}
+
+
+?>
