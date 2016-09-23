fe6e0749aa5c197ebbef692b8ed477a753cd8e1f
ASzczesna <szczesna.aleksandra@gmail.com>
2016-09-23 21:55:03 +0200

    ustawianie favicony na podstawie pliku w folderze wtyczki

---
+++ MyFaviconSetter/MyFaviconSetter.php
@@ -0,0 +1,22 @@
+<?php
+/*
+Plugin Name: MyFaviconSetter
+Plugin URI: 
+Description: Ustawia faviconê na podstawie za³¹czonego w folderze pliku
+Version: 0.1 
+Requires at least: 4.6
+Author: ASz
+Author URI: http://mojswiat.eu
+License: GPL2
+*/
+
+add_action( 'wp_head', 'MyFaviconSetter' );
+
+function MyFaviconSetter() { 
+
+	$icon_link = plugins_url( 'favicon.ico', __FILE__ );
+	?> 
+	<link rel="shortcut icon" href="<?php echo $icon_link; ?>" />
+	<?php 
+}
+?>
